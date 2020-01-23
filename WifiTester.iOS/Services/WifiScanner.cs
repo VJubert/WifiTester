0b88388356314fb746936db038419e5f6e24b573
Valentin Jubert <valentin.jubert@ideine.fr>
2020-01-23 12:07:25 +0100

    update documentation for iOS

--- WifiTester.iOS/Services/WifiScanner.cs
+++ WifiTester.iOS/Services/WifiScanner.cs
@@ -7,15 +7,23 @@ using WifiTester.Services;
 
 namespace WifiTester.iOS.Services
 {
+    // ios need require to apple :
+    // https://developer.apple.com/contact/request/hotspot-helper/
+    // https://stackoverflow.com/questions/9684341/iphone-get-a-list-of-all-ssids-without-private-library/52473480#52473480
+    // https://developer.apple.com/documentation/networkextension/nehotspothelper/1618965-register
+    // after that you can get network list when user goes to the wifi system page
+    // you can ask to connect to a network if you have password and ssid
+    // futhermore, you can not open general settings of device, only applications settings
+
     public class WifiScanner : IWifiScanner
     {
-        public WifiScanner()
-        {
-        }
+        public WifiScanner() { }
 
         public Task<List<WifiInformation>> ScanForWifi()
         {
-            var res = NEHotspotHelper.Register(null, DispatchQueue.DefaultGlobalQueue, x =>
+            var queue = new DispatchQueue("network queue");
+
+            var res = NEHotspotHelper.Register(null, queue, x =>
                {
                    Console.WriteLine("handling");
 
@@ -25,7 +33,7 @@ namespace WifiTester.iOS.Services
                    }
                });
 
-            Console.WriteLine($"Results {res}");
+            Console.WriteLine($"Return {res}");
 
             return Task.FromResult((List<WifiInformation>)null);
         }
